fb73536aad17782e2c15df67f0d37d95f144f0f7
Mark Baker <user.email>
2021-05-27 22:05:52 -0500

    gg

--- models/user.js
+++ models/user.js
@@ -1,21 +1,14 @@
-const { TooManyRequests } = require("http-errors");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
   admin: {
     type: Boolean,
     default: false,
   },
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
