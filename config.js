fa015244e9c36e237a20d6449311f73fb39d7684
Mark Baker <user.email>
2021-06-04 22:05:13 -0500

    Facebook Auth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "3102354643369436",
+    clientSecret: "768c6b26c4840b241bf09e0410227417",
+  },
 };
